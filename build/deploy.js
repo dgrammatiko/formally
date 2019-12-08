76b7ddb41f535588e60ce6204288f513843c60a8
dgrammatiko <d.grammatiko@gmail.com>
2019-12-08 18:06:26 +0100

    missing parts for deploy

---
+++ build/deploy.js
@@ -0,0 +1,26 @@
+const ghpages = require('gh-pages');
+
+const token = process.env.GH_TOKEN;
+
+ghpages.publish(
+  'gh-pages/dist',
+  {
+    repo: `https://${token}@github.com/dgrammatiko/dgrammatiko.online.git`,
+    // silent: true,
+    dotfiles: true,
+    message: '🚢 it!',
+    user: {
+      name: 'Deploy Bot',
+      email: 'd.grammatiko@gmail.com',
+    },
+  },
+  err => {
+    if (err) {
+      console.error('FAILURE');
+      const tokenRegex = new RegExp(token, 'gm');
+      console.error(err.message.replace(tokenRegex, 'GH_TOKEN'));
+      process.exit(2);
+    }
+    console.log('Deployed');
+  },
+);
