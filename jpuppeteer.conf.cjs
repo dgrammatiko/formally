25786bf25a76f37838a855a9279830c29ec7afd1
dGrammatiko <d.grammatiko@gmail.com>
2020-06-11 10:59:46 +0200

    Update jpuppeteer.conf.cjs

--- jpuppeteer.conf.cjs
+++ jpuppeteer.conf.cjs
@@ -1,7 +1,7 @@
 //   const configPath = process.env.JEST_PUPPETEER_CONFIG || 'jest-puppeteer.config.js';
 module.exports = {
     launch: {
-        headless: false, // process.env.HEADLESS !== 'false',
+        headless: process.env.HEADLESS !== 'false',
         slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
         devtools: true
     },
